fcaf13f90b06f5a678057ba00a684ee9da921647
Joe <jihwan3331@gmail.com>
2023-07-07 19:10:06 +0900

    asd

--- jsIntroNotes/app.js
+++
@@ -1,9 +0,0 @@
-const arr = Array(100).fill(0);
-const arr3 = [...Array(100).keys()];
-const arr2 = Array(100)
-  .fill(0)
-  .map((_, i) => i * 2);
-
-for (const [i, val] of arr2.entries()) {
-  console.log("Index: ", i, "Value: ", val);
-}
